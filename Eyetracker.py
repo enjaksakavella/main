243ba6de2c296707189d9cdadd107cc22e492ead
alastaa <antti.alastalo@aalto.fi>
2016-08-06 17:39:07 +0300

    e

--- Eyetracker.py
+++ Eyetracker.py
@@ -38,12 +38,13 @@ class Eyetracker:
 		return False
 		
 	def getBoundingRectangle(self):
-		frame = self.takeSnapShot(gray = True, blurr = True)
 		eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-		eyes = eye_cascade.detectMultiScale(frame,1.2,1,minSize=(100, 100))
+		
 		eye_found = False
 		
 		while not eye_found:
+			frame = self.takeSnapShot(gray = True, blurr = True)
+			eyes = eye_cascade.detectMultiScale(frame,1.2,1,minSize=(100, 100))
 			for (x,y,w,h) in eyes:
 				eye = frame[y:(y+h), x:(x+w)]
 				cv2.imshow('img4',eye)
@@ -67,8 +68,9 @@ class Eyetracker:
 		x,y,w,h = self.eyeRec[0],self.eyeRec[1],self.eyeRec[2],self.eyeRec[3]
 		for i in range(40,90,2):
 			ret, thresh = cv2.threshold(frame,i,250,cv2.THRESH_BINARY)
+			#thresh = cv2.dilate(thresh,np.ones((5,5),np.uint8),iterations =1)
 			thresh = cv2.erode(thresh,np.ones((5,5),np.uint8),iterations =4)
-			thresh = cv2.dilate(thresh,np.ones((5,5),np.uint8),iterations =2)
+			thresh = cv2.dilate(thresh,np.ones((5,5),np.uint8),iterations = 2)
 			cv2.imshow('pupildetection',thresh[y:(y+h),x:(x+w)])
 			contours, hierarchy = cv2.findContours(thresh[y:(y+h),x:(x+w)],cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
 			#print(len(contours))
@@ -91,6 +93,8 @@ class Eyetracker:
 				center = cv2.moments(largeBlob)
 				cx,cy = int(center['m10']/center['m00']), int(center['m01']/center['m00'])
 				self.pupil = (x+cx,y+cy)
+				if cv2.waitKey(1) & 0xFF == ord('l'):
+					self.loadimages(i)
 				#cv2.circle(frame,(int(x+xx),int(y+yy)),int(10),(0,0,255),2)
 				#print(i)
 				break
@@ -115,3 +119,20 @@ class Eyetracker:
 		cv2.putText(frame,string,(x,y), font, 1,(255,255,255),2)
 		cv2.line(frame,(self.center[0],self.center[1]-100),(self.center[0],self.center[1]+100),(255,0,0),5)
 		cv2.imshow('img1',frame)
+		
+		
+	def loadimages(self,t):
+		x,y,w,h = self.eyeRec[0],self.eyeRec[1],self.eyeRec[2],self.eyeRec[3]
+		image1 = self.takeSnapShot()
+		gray1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+		blur1 = cv2.medianBlur(gray1,5)
+		ret, thresh1 = cv2.threshold(blur1,t,250,cv2.THRESH_BINARY)
+		thresh2 = cv2.erode(thresh1,np.ones((5,5),np.uint8),iterations =4)
+		thresh3 = cv2.dilate(thresh2,np.ones((5,5),np.uint8),iterations =2)
+		cv2.imwrite('image1.jpeg',image1)
+		cv2.imwrite('gray1.jpeg',gray1)
+		cv2.imwrite('blur1.jpeg',blur1)
+		cv2.imwrite('thresh1.jpeg',thresh1[y:(y+h),x:(x+w)])
+		cv2.imwrite('thresh2.jpeg',thresh2[y:(y+h),x:(x+w)])
+		cv2.imwrite('thresh3.jpeg',thresh3[y:(y+h),x:(x+w)])
+		
--- eyetrack_main.py
+++ eyetrack_main.py
@@ -17,7 +17,7 @@ def main():
 	tracker.getBoundingRectangle()
 	tracker.calibrate()
 	
-	print(tracker.blink_value)
+	#print(tracker.blink_value)
 	
 	maxRotationVal = 40
 	minRotationVal = -40
@@ -40,15 +40,15 @@ def main():
 			blinktimer = 0
 		start = time.time()
 		
-		print(blinktimer)
+		#print(blinktimer)
 		if blinktimer > 0.5:
 			if forwardmode:
 				forwardmode = False
 			else:
 				forwardmode = True
-			blinktimer = 0
-		r = 85
-		f = 85
+			blinktimer = -9999
+		r = 192
+		f = 64
 		
 		if not tracker.blink:
 			tracker.trackPupil()
@@ -59,19 +59,19 @@ def main():
 				dist = minRotationVal
 
 			#print(dist)
-			i = 85
 			if dist < 0:
-				i = i - 21*dist/minRotationVal
+				r = r - 63*dist/minRotationVal
 			if dist > 0:
-				i = i + 26*dist/maxRotationVal
+				r = r + 63*dist/maxRotationVal
 			if forwardmode:
 				print("FORWAAAAAARD")
-				f = 100
+				f = 80
 			
 			
-		#ser.write(i)
+		#ser.write(chr(r))
+		#ser.write(chr(f))
 		
-		#print(i)
+		print(r)
 		tracker.draw()
 		
 		if cv2.waitKey(1) & 0xFF == ord('q'):
